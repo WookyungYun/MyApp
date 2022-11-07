42c96a09a2f52e65a2eb3116a479c6c5ed362aac
WookyungYun <gyong1540@gmail.com>
2022-11-07 20:25:00 +0900

    feat: add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+
+export default function Custom404() {
+  return (
+    <>
+      <Box
+        height="100vh"
+        display="flex"
+        position="relative"
+        alignItems="center"
+        justifyContent="center"
+        fontSize="50px">
+        Page Not Found
+      </Box>
+    </>
+  );
+}
