0843250c33d0a16f2e80d01400f1f1e588dffb08
WookyungYun <gyong1540@gmail.com>
2022-11-07 20:06:48 +0900

    feat:  add Link

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,21 +1,22 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, Link, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { Router, useRouter } from "next/router";
 import { useRef } from "react";
 import { useForm } from "react-hook-form";
 
 export default function Register() {
+  const router = useRouter();
   const {
     register,
     watch,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
   } = useForm();
   const password = useRef({});
   password.current = watch("password", "");
-  console.log(password.current);
   const onSubmit = (data) => {
     console.log(data);
-    console.log("?", errors.email);
+    if (isSubmitSuccessful) router.push("/login");
   };
   const emailRegRex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
@@ -33,56 +34,56 @@ export default function Register() {
           Welcome!
         </Typography>
       </Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          fullWidth
-          autoFocus
-          label="Email Address"
-          name="email"
-          sx={{ marginBottom: 10 }}
-          {...register("email", {
-            required: "이메일은 필수입니다.",
-            validate: (value) =>
-              emailRegRex.test(value) || "이메일 형식에 맞게 입력해주세요.",
-          })}
-          error={Boolean(errors.email)}
-          helperText={errors.email?.message}></TextField>
-        <TextField
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          sx={{ marginBottom: 10 }}
-          {...register("password", {
-            required: "비밀번호는 필수입니다.",
-            minLength: { value: 10, message: "최소 8자 이상 입력해주세요." },
-          })}
-          error={Boolean(errors.password)}
-          helperText={errors.password?.message}></TextField>
 
-        <TextField
+      <TextField
+        fullWidth
+        autoFocus
+        label="Email Address"
+        name="email"
+        sx={{ marginBottom: 10 }}
+        {...register("email", {
+          required: "이메일은 필수입니다.",
+          validate: (value) =>
+            emailRegRex.test(value) || "이메일 형식에 맞게 입력해주세요.",
+        })}
+        error={Boolean(errors.email)}
+        helperText={errors.email?.message}></TextField>
+      <TextField
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        sx={{ marginBottom: 10 }}
+        {...register("password", {
+          required: "비밀번호는 필수입니다.",
+          minLength: { value: 8, message: "최소 8자 이상 입력해주세요." },
+        })}
+        error={Boolean(errors.password)}
+        helperText={errors.password?.message}></TextField>
+
+      <TextField
+        fullWidth
+        name="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        sx={{ marginBottom: 15 }}
+        {...register("confirmPassword", {
+          required: "비밀번호는 필수입니다.",
+          validate: (value) =>
+            value === password.current || "비밀번호가 일치하지 않습니다.",
+        })}
+        error={Boolean(errors.confirmPassword)}
+        helperText={errors.confirmPassword?.message}></TextField>
+      <div>
+        <Button
+          onClick={handleSubmit(onSubmit)}
           fullWidth
-          name="confirmPassword"
-          label="Confirm Password"
-          type="password"
-          sx={{ marginBottom: 15 }}
-          {...register("confirmPassword", {
-            required: "비밀번호는 필수입니다.",
-            validate: (value) =>
-              value === password.current || "비밀번호가 일치하지 않습니다.",
-          })}
-          error={Boolean(errors.confirmPassword)}
-          helperText={errors.confirmPassword?.message}></TextField>
-        <div>
-          <Button
-            fullWidth
-            type="submit"
-            variant="contained"
-            sx={{ marginBottom: 10 }}>
-            Sign Up
-          </Button>
-        </div>
-      </form>
+          type="submit"
+          variant="contained"
+          sx={{ marginBottom: 10 }}>
+          Sign up
+        </Button>
+      </div>
     </Box>
   );
 }
