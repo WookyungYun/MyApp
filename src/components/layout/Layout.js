6e48dbc3b90440e2edc532b3fb7aa12cf48b736d
WookyungYun <gyong1540@gmail.com>
2022-11-11 17:51:50 +0900

    chore: nav수정

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
+import NavBar from "./_fragments/NavBar";
 
 export default function Layout({ children }) {
   return (
     <Box height="100vh">
-      <Box maxWidth="440px" margin="auto">
-        {children}
-      </Box>
+      <NavBar />
+      <Box margin="auto">{children}</Box>
     </Box>
   );
 }
--- src/components/layout/_fragments/NavBar.js
+++ src/components/layout/_fragments/NavBar.js
@@ -1,10 +1,32 @@
-import Link from "next/link";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function NavBar() {
+  const onClickWeb = () => {
+    alert("준비중입니다.");
+  };
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-    </div>
+    <Box display="flex" width="100%" padding="10px">
+      <Button
+        sx={{ width: "50%", height: 50 }}
+        variant="contained"
+        display="flex"
+        justifyContent="center"
+        alignItems="center">
+        <Typography fontSize="30px" fontWeight="900" color="white">
+          APP
+        </Typography>
+      </Button>
+      <Button
+        sx={{ width: "50%", height: 50 }}
+        variant="contained"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        onClick={onClickWeb}>
+        <Typography fontSize="30px" fontWeight="900" color="white">
+          WEB
+        </Typography>
+      </Button>
+    </Box>
   );
 }
